6b17fb65cb98181bf6fff872a4ec470a4da6e9dd
Ali Emre <aliemree.y@hotmail.com>
2024-02-26 17:12:16 +0300

    Add project files.

---
+++ FutureCodersWebApi/Models/Course.cs
@@ -0,0 +1,12 @@
+﻿namespace FutureCodersWebApi.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public String? CourseName { get; set; }
+        public String? CourseDescription { get; set; }
+        public String? CourseThumbnail { get; set; }
+        public bool IsRequire { get; set; }
+        public int Rank { get; set; }
+    }
+}
